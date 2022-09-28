25d599f599c82993dfb22db0eaeb1015ba488173
Niklas Lindblad <niklas.o.lindblad@gmail.com>
2022-09-28 10:40:43 +0200

    simple web bomber app

---
+++ Bomber/Bomber/Bomber/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using var client = new HttpClient();
+
+var timesToRequst = 100;
+
+for (int requestRepetitions = 0; requestRepetitions < timesToRequst; requestRepetitions++)
+{
+    var result = await client.GetAsync("https://bestalpineappdemo.azurewebsites.net/", HttpCompletionOption.ResponseContentRead);
+    Console.WriteLine(result.StatusCode);
+}
